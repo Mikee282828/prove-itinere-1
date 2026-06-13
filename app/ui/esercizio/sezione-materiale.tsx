f2d83389463838d2f0fe1ae0c3dfae057b62e559
MikeMac <micheledong2803@gmail.com>
2026-06-14 00:12:37 +0200

    Link relative to current url

--- app/ui/esercizio/sezione-materiale.tsx
+++ app/ui/esercizio/sezione-materiale.tsx
@@ -14,28 +14,28 @@ export default function SezioneMateriale({
     return (
       <div className="p-8">
         <Link
-          href="http://localhost:3000/esercizio?data=2025-12-01&inizio=10%3A40&fine=15%3A30"
+          href="/esercizio?data=2025-12-01&inizio=10%3A40&fine=15%3A30"
           className="text-blue-500 hover:text-blue-400"
         >
           2025-12-01 10:40 ~ 15:31
         </Link>
         <br />
         <Link
-          href="http://localhost:3000/esercizio?data=2025-12-01&inizio=15%3A30&fine=20%3A30"
+          href="/esercizio?data=2025-12-01&inizio=15%3A30&fine=20%3A30"
           className="text-blue-500 hover:text-blue-400"
         >
           2025-12-01 15:30 ~ 20:30
         </Link>
         <br />
         <Link
-          href="http://localhost:3000/esercizio?data=2025-12-01&inizio=06%3A40&fine=10%3A41"
+          href="/esercizio?data=2025-12-01&inizio=06%3A40&fine=10%3A41"
           className="text-blue-500 hover:text-blue-400"
         >
           2025-12-01 6:40 ~ 10:41
         </Link>
         <br />
         <Link
-          href="http://localhost:3000/esercizio?data=2025-12-01&inizio=15%3A29&fine=20%3A30"
+          href="/esercizio?data=2025-12-01&inizio=15%3A29&fine=20%3A30"
           className="text-blue-500 hover:text-blue-400"
         >
           2025-12-01 15:29 ~ 20:30
