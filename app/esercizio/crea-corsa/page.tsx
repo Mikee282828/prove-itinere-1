af75c42c136276900e26bafbb1d08fbd6b55480e
MikeMac <micheledong2803@gmail.com>
2026-06-20 11:00:44 +0200

    crea corsa actions - only consolelog

--- app/esercizio/crea-corsa/page.tsx
+++ app/esercizio/crea-corsa/page.tsx
@@ -1,11 +1,14 @@
-import { ConvoglioRaggruppato, fetchComposizioni } from "@/app/lib/data";
+import { ConvoglioRaggruppato, fetchComposizioni, fetchStazioni, provaFetch } from "@/app/lib/data";
+import { Stazione } from "@/app/lib/definitions";
 import Form from "@/app/ui/esercizio/crea-corsa";
 
 export default async function Page() {
   const convogliDisponibili:ConvoglioRaggruppato[] = await fetchComposizioni();
+  const stazioni : Stazione[] = await fetchStazioni();
   return (
     <main>
-      <Form convogli={convogliDisponibili}/>
+      <Form convogli={convogliDisponibili} stazioni={stazioni}/>
+
     </main>
   );
 }
