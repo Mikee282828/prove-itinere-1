3f44227b9fbdb5af2e5510b24259d602f020f124
micLaptop <micheledong2803@gmail.com>
2026-07-13 18:05:22 +0200

    FetchMaterialeEsercizio: search fix

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,8 +11,8 @@ export async function fetchStazioni(): Promise<Stazione[]> {
       ORDER BY stazione.distanza_capolinea ASC`;
     return stazioni as Stazione[];
   } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch data.');
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch data.");
   }
 }
 
@@ -24,36 +24,42 @@ export async function fetchMateriale(): Promise<MaterialeRotabile[]> {
       ORDER BY tipologia`;
     return materiali as MaterialeRotabile[];
   } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch data.');
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch data.");
   }
 }
 
-export async function fetchMaterialeEsercizio(data: string, inizio: string, fine: string): Promise<MaterialeRotabile[]> {
-
+export async function fetchMaterialeEsercizio(
+  data: string,
+  inizio: string,
+  fine: string,
+): Promise<MaterialeRotabile[]> {
   if (!data || !inizio || !fine || inizio > fine) return [];
 
   try {
     const materiali = await sql`
-      SELECT DISTINCT mat.* FROM materiale_rotabile AS mat
+      SELECT mat.* 
+      FROM materiale_rotabile AS mat
       WHERE mat.id NOT IN(
-      SELECT DISTINCT comp.id_mat 
-      FROM composizione AS comp
-      INNER JOIN convoglio ON comp.convoglio = convoglio.id
-      INNER JOIN treno ON convoglio.id = treno.convoglio
-      INNER JOIN traccia_passata as traccia ON treno.data = traccia.data AND treno.codice = traccia.treno
-      WHERE traccia.data = ${data}
-        AND (traccia.orario_arrivo > ${inizio} AND traccia.orario_arrivo<${fine})
-  )`;
+        SELECT comp.id_mat as id
+        FROM composizione AS comp
+        INNER JOIN convoglio ON comp.convoglio = convoglio.id
+        INNER JOIN treno ON convoglio.id = treno.convoglio
+        INNER JOIN traccia_corrente as traccia ON treno.data = traccia.data AND treno.codice = traccia.treno
+        WHERE traccia.data = ${data}
+        GROUP BY id_mat
+        HAVING MAX(traccia.orario_arrivo)>${inizio} AND MIN(traccia.orario_partenza)<${fine}
+        );
+        `;
     return materiali as MaterialeRotabile[];
   } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch data.');
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch data.");
   }
 }
 
 export type ConvoglioRaggruppato = {
-  convoglio: string;   // l'ID del convoglio (es: '1', '2')
+  convoglio: string; // l'ID del convoglio (es: '1', '2')
   materiali: string[]; // l'array di stringhe (es: ['Cavour', 'B1'])
 };
 
@@ -66,22 +72,24 @@ export async function fetchComposizioni(): Promise<ConvoglioRaggruppato[]> {
     ORDER BY convoglio;`;
     return composizioni as ConvoglioRaggruppato[];
   } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch data.');
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch data.");
   }
 }
 
-export async function fetchTracceCorrenti(data: string): Promise<TracciaCorrente[]> {
+export async function fetchTracceCorrenti(
+  data: string,
+): Promise<TracciaCorrente[]> {
   try {
     const tracce = await sql`
     SELECT * from traccia_corrente
     WHERE data = ${data} AND data >= NOW()::DATE
     ORDER BY treno ASC;
-    `
+    `;
     console.log(tracce);
-    return tracce as TracciaCorrente[]
+    return tracce as TracciaCorrente[];
   } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch data.');
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch data.");
   }
 }
--- app/ui/esercizio/sezione-materiale.tsx
+++ app/ui/esercizio/sezione-materiale.tsx
@@ -14,24 +14,30 @@ export default function SezioneMateriale({
     return (
       <div className="p-8">
         <Link
-          href="/esercizio?data=2025-12-01&inizio=10%3A40&fine=15%3A30"
+          href="/esercizio?data=2070-01-01&inizio=10%3A40&fine=10%3A50"
           className="text-blue-500 hover:text-blue-400"
         >
-          2025-12-01 10:40 ~ 15:31
+          2070-01-01 10:40 ~ 10:50 (esterno - interno)
         </Link>
         <br />
         <Link
-          href="/esercizio?data=2025-12-01&inizio=15%3A30&fine=20%3A30"
+          href="/esercizio?data=2070-01-01&inizio=10%3A56&fine=10%3A56"
           className="text-blue-500 hover:text-blue-400"
         >
-          2025-12-01 15:30 ~ 20:30
+          2070-01-01 10:56 ~ 10:56 (interno)
         </Link>
         <br />
         <Link
-          href="/esercizio?data=2025-12-01&inizio=06%3A40&fine=10%3A41"
+          href="/esercizio?data=2070-01-01&inizio=15%3A29&fine=15%3A32"
           className="text-blue-500 hover:text-blue-400"
         >
-          2025-12-01 6:40 ~ 10:41
+          2070-01-01 15:29 ~ 15:32 (interno - esterno)
+        </Link>
+        <Link
+          href="/esercizio?data=2070-01-01&inizio=15%3A30&fine=15%3A32"
+          className="text-blue-500 hover:text-blue-400"
+        >
+          2070-01-01 15:30 ~ 15:32 (esterno)
         </Link>
         <br />
         <Search />
