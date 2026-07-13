5b5932c89d8f32908021bd2195b5894b5ec32d9f
micLaptop <micheledong2803@gmail.com>
2026-07-13 15:48:58 +0200

    UI element: removed link

--- app/ui/esercizio/sezione-materiale.tsx
+++ app/ui/esercizio/sezione-materiale.tsx
@@ -34,12 +34,6 @@ export default function SezioneMateriale({
           2025-12-01 6:40 ~ 10:41
         </Link>
         <br />
-        <Link
-          href="/esercizio?data=2025-12-01&inizio=15%3A29&fine=20%3A30"
-          className="text-blue-500 hover:text-blue-400"
-        >
-          2025-12-01 15:29 ~ 20:30
-        </Link>
         <Search />
         <Materiali materialeRotabile={materialeRotabile} />
       </div>
